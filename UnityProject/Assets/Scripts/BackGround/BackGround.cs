d662c3ae2dfb0abade9143b8c96e73466166be47
DESKTOP-05PR4TA\Miyazawa.Fuki <tyukajin2014@gmail.com>
2018-02-22 15:02:10 +0900

    スクロール修正 # 修正点 スクロールしないのをしっぱなしにしたお

--- UnityProject/Assets/Scripts/BackGround/BackGround.cs
+++ UnityProject/Assets/Scripts/BackGround/BackGround.cs
@@ -8,31 +8,14 @@ public class BackGround : MonoBehaviour {
     [Range(0, 1)]
     public float speed;
 
-    bool isMove = false;
-
 	// Use this for initialization
 	void Start () {
-        isMove = false;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        if(!isMove)
-        {
-            return;
-        }
         float scroll = Mathf.Repeat(Time.time* speed, 1);
         Vector2 offset = new Vector2(0, scroll);
         render.sharedMaterial.SetTextureOffset("_MainTex",offset);
 	}
-
-    public void MoveStop()
-    {
-        isMove = false;
-    }
-
-    public void MoveStart()
-    {
-        isMove = true;
-    }
 }
