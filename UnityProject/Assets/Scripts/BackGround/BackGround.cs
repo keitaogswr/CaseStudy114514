cbd972a83b5f6ecc8a45ce171cf84cf47f5e8dbd
DESKTOP-05PR4TA\Miyazawa.Fuki <tyukajin2014@gmail.com>
2018-02-22 14:36:36 +0900

    BackGround # 変更点 背景をスクロールするスクリプトの作成 背景をプレハブ化 背景用のマテリアルの作成 →作成時にMaterialsフォルダを作成したお

---
+++ UnityProject/Assets/Scripts/BackGround/BackGround.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour {
+
+    public Renderer render;
+    [Range(0, 1)]
+    public float speed;
+
+    bool isMove = false;
+
+	// Use this for initialization
+	void Start () {
+        isMove = false;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(!isMove)
+        {
+            return;
+        }
+        float scroll = Mathf.Repeat(Time.time* speed, 1);
+        Vector2 offset = new Vector2(0, scroll);
+        render.sharedMaterial.SetTextureOffset("_MainTex",offset);
+	}
+
+    public void MoveStop()
+    {
+        isMove = false;
+    }
+
+    public void MoveStart()
+    {
+        isMove = true;
+    }
+}
