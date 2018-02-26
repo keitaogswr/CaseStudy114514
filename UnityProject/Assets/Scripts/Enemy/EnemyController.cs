add193f2980a4dc0c94e9c7b4ed962184854277e
keitaogswr <xevolt1027.kamendam@gmail.com>
2018-02-26 16:48:47 +0900

    エネミーが弾を発射するように、状態遷移の追加

--- UnityProject/Assets/Scripts/Enemy/EnemyController.cs
+++ UnityProject/Assets/Scripts/Enemy/EnemyController.cs
@@ -10,11 +10,25 @@ public class EnemyController : MonoBehaviour {
     protected EnemyBulletPattern bulletPattern;         // エネミーの攻撃パターン
 
     [SerializeField]
-    public float startMoveTime = 2.0f;               // 初期位置に移動するまでの時間
+    protected float startMoveTime = 2.0f;               // 初期位置に移動するまでの時間
+    [SerializeField]
+    protected Bullet bullet;                            // 弾
+    protected float bulletTime;                         // 弾の発射までの残り時間
+    [SerializeField]
+    protected float bulletInterval = 3.0f;              // 弾の発射間隔
+
+    private SpriteRenderer MainSpriteRenderer;          // 使用スプライト
+    [SerializeField]
+    protected Sprite normalSprite;                      // 通常時テクスチャ
+    [SerializeField]
+    protected Sprite diffusionSprite;                   // 拡散時テクスチャ
 
     // Use this for initialization
     void Start()
     {
+        // このobjectのSpriteRendererを取得
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        MainSpriteRenderer.sprite = normalSprite;
         enemyPattern = new EnemyPatternStart();
         enemyPattern.Init(this);
         bulletPattern = new EnemyBulletPatternNone();
@@ -39,8 +53,30 @@ public class EnemyController : MonoBehaviour {
         bulletPattern = pattern;
     }
 
+    // エネミーの当たり判定
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        string layerName = LayerMask.LayerToName(collider.gameObject.layer);
+        // 衝突：MP4弾
+        if (layerName == "MP4Bullet")
+        {
+            // 行動変化
+            enemyPattern.ChangePattern(this);
+            Destroy(collider.gameObject);
+        }
+    }
+
+    public void ChangeSpriteDiffusion()
+    {
+        MainSpriteRenderer.sprite = diffusionSprite;
+    }
+
     public Vector3 NextPoint { get{ return nextPoint; } set { nextPoint = value; } }
     public float StartMoveTime { get { return startMoveTime; } set { startMoveTime = value; } }
     public EnemyPattern EnemyPattern { get { return enemyPattern; } set { enemyPattern = value; } }
     public EnemyBulletPattern BulletPattern { get { return bulletPattern; } set { bulletPattern = value; } }
+    public Bullet Bullet { get { return bullet; } set { bullet = value; } }
+    public float BulletTime { get { return bulletTime; } set { bulletTime = value; } }
+    public float BulletInterval { get { return bulletInterval; } set { bulletInterval = value; } }
+    public Sprite DiffusionSprite { get { return DiffusionSprite; } set { DiffusionSprite = value; } }
 }
